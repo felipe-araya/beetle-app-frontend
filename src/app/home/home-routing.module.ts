62c5f5458564317ac692e4d8336bc4a7d57ad6e8
fa091 <felipe.saldivar@uni-rostock.de>
2021-06-08 01:24:18 +0200

    Add home-routing.module...

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+import {Role} from '@core/role.model';
+import {RoleGuardService} from '@core/role-guard.service';
+
+import {HomeComponent} from './home.component';
+
+
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent,
+    
+   
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule {
+}
