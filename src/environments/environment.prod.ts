de03c9765163b0bab6918ba787de0a96e36231cc
fa091 <felipe.saldivar@uni-rostock.de>
2021-06-07 22:35:30 +0200

    Check until server.js

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+import {name, version} from '../../package.json';
+
 export const environment = {
-  production: true
+  production: true,
+  NAME: name,
+  VERSION: version
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,16 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import {name, version} from '../../package.json';
+
 export const environment = {
-  production: false
+  production: false,
+  NAME: name,
+  VERSION: version,
+  REST_USER: 'http://localhost:8081',
+  REST_CORE: 'http://localhost:8082',
+  REST_CUSTOMER_SUPPORT: 'http://localhost:8083'
+
 };
 
 /*
