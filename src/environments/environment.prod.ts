4e24527f4d2976f33981702458395f1d33e03be5
fa091 <felipe.saldivar@uni-rostock.de>
2021-06-08 13:19:00 +0200

    Try post - settings Chrome

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: true,
   NAME: packageInfo.name,
   VERSION: packageInfo.version,
-  REST_USER: 'http://beetle-app-backend.herokuapp.com',
-  REST_CORE: 'http://beetle-app-backend.herokuapp.com',
-  REST_CUSTOMER_SUPPORT: 'http://beetle-app-backend.herokuapp.com'
+  REST_USER: 'https://beetle-app-backend.herokuapp.com',
+  REST_CORE: 'https://beetle-app-backend.herokuapp.com',
+  REST_CUSTOMER_SUPPORT: 'https://beetle-app-backend.herokuapp.com'
 };
