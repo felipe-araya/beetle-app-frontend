3dcb9c3fb178a933cd95d23797ff8265b502dcd2
fa091 <felipe.saldivar@uni-rostock.de>
2021-06-08 08:19:47 +0200

    Try routing home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeModule } from './home/home.module';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: 'home', loadChildren: () => import('./home/home.module').then(module => module.HomeModule)}, // lazy load
+
+ 
 
 
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: true,
   NAME: packageInfo.name,
   VERSION: packageInfo.version,
-  REST_USER: 'https://betca-tpv-user.herokuapp.com',
-  REST_CORE: 'https://betca-tpv-core.herokuapp.com',
-  REST_CUSTOMER_SUPPORT: 'https://betca-tpv-customer-support.herokuapp.com'
+  REST_USER: 'https://beetle-app-frontend.herokuapp.com',
+  REST_CORE: 'https://beetle-app-frontend.herokuapp.com',
+  REST_CUSTOMER_SUPPORT: 'https://beetle-app-frontend.herokuapp.com'
 };
